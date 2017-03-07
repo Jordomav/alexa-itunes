7019a4258aee67311b3a9cb2ad6eaf4a4678ef09
Jordan Mavrogeorge <jordomav@gmail.com>
2017-03-06 16:44:01 -0800

    making requests to itunes

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
 var alexa = require('alexa-app');
 var app = new alexa.app();
+var request = require('request');
 
 /**
  * LaunchRequest.
  */
 app.launch(function(request,response) {
-	response.say('Hey there fancy pants!');
-	response.card("Hey there fancy pants!","This is an example card");
+	response.say('What would you like me to find');
 });
 
 
-/**
- * IntentRequest.
- */
-app.intent('number',
-  {
-    'slots':{'number':'NUMBER'},
-    'utterances':[ 'say the number {1-100|number}' ]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    response.say('You asked for the number '+number);
-    response.shouldEndSession(true);
-    response.send();
-  }
-);
-
-
-/**
- * IntentRequest w/ asynchronous response.
- */
-app.intent('checkStatus', 
+app.intent('movie',
 	{
-    	'utterances':[ 
-    		'status check', 'what is the status', 'tell me the status'
-    	]
-  	},
-	function(request,response) {
-		setTimeout(function() {		// simulate an async request
-
-	        // This is async and will run after a brief delay
-	        response.say('Status is operational, mam!');
-	    
-	        // Must call send to end the original request
-	        response.send();
-		
-		}, 250);
-
-	    // Return false immediately so alexa-app doesn't send the response
-	    return false;
+		'slots' : {'movie': 'MOVIE'},
+		'utterances' : ['find the movie {movie}', 'show me the movie {movie}']
+	},
+	function (req, res) {
+		var movie = req.slot('movie');
+		request('https://itunes.apple.com/search?term=' + movie + '&media=movie', function (error, response, body) {
+			if (error) {
+				res.say('Could not find search results for ' + movie);
+				res.shouldEndSession(true);
+				res.send();
+			} else {
+				res.say('Here is what I found for ' + movie + '. Please look at the Alexa app for more info');
+			}
+		});
 	}
 );
+
+app.intent('music',
+	{
+		'slots' : {'song' : 'SONG'},
+		'utterances' : ['find me the song {song}']
+	},
+	function (req, res) {
+		var song = req.slots('song');
+		request('https://itunes.apple.com/search?term=' + song + '&media=music', function (error, response, body) {
+			if (error) {
+				res.say('Could not find search results for ' + song);
+				res.shouldEndSession(true);
+				res.send();
+			} else {
+				res.say('Here is what I found for ' + song + '. Please look at the Alexa app for more info');
+			}
+		});
+	}
+);
+
 
 
 /**
